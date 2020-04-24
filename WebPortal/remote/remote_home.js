ba85a189365a6b24144191289bfdd0e648eef256
TARATESTER <63991659+TARATESTER@users.noreply.github.com>
2020-04-24 16:40:17 +0630

    Custom error message is displayed
    Custom error message is displayed if remote server is not pingable.

--- WebPortal/remote/remote_home.js
+++ WebPortal/remote/remote_home.js
@@ -1,14 +1,15 @@
 /*
- * Copyright (c) 2015 - 2019 - Tara Aung Co., Ltd - All Rights Reserved.
+ * Copyright (c) 2015 - 2020 - Tara Aung Co., Ltd - All Rights Reserved.
  * Tara Systems Project - Proprietary and confidential. www.taraaung.com
  */
 
-/* global getUrlParam */
+/* global getUrlParam:writable */
 var loadingImg, infoMessage;
 
+// eslint-disable-next-line no-unused-vars
 function init() {
-    loadingImg = document.getElementById('loading_img');
-    infoMessage = document.getElementById('message');
+    loadingImg = document.getElementById("loading_img");
+    infoMessage = document.getElementById("message");
     var shopName = getUrlParam("shop_name");
     var systemType = getUrlParam("system_type");
     var hostName = getUrlParam("host_name");
@@ -29,7 +30,22 @@ function init() {
                 function success(response) {
                     var params = "?host_name=" + hostName + "&sn=" + shopName;
                     params += "&icft=" + (response ? response.isClientFirstTime : "true");
-                    window.location.replace(serverPublicUrl + params);
+                    httpGetAsync(serverPublicUrl + "/client_info",
+                        function success(response) {
+                            window.location.replace(serverPublicUrl + params);
+                        }, function error(errorMsg) {
+                            showErrorMessage(
+                                "ShopName = " + shopName + "</br>HostName = " + hostName + "</br>Systemtype = " + systemType + "</br>" +
+                                "Please confirm above information is correct!</br></br>" +
+                                "1. Please check internet connection is ok at both Server and client machine.</br>" +
+                                "2. Check that Cloud Access status is online at Server Logs.</br>" +
+                                "3. At server machine, Click on \"Restart Cloud Access\" (Home Screen -> About -> Server Logs).</br>" +
+                                "4. Make sure Server and Client date and time are correct and the same.</br></br>" +
+                                "1. Cloud Services မရတော့တဲ့အခါServer နဲ့ Client စက်တွေမှာ Internet ရှိမရှိစစ်ပေးပါ။</br>" +
+                                "2. Server စက်ရဲ့ Server Log မှာ online ဖြစ်ပီးစိမ်းနေလား စစ်ပေးပါ။</br>" +
+                                "3. Server စက်ရဲ့ about icon ထဲက server log တွင် \"Restart Cloud Access\" ကို နှိပ်၍ Cloud Service ကို restartချပေးပါ။</br>" +
+                                "4. Server နှင့် Client စက်များ၏ အချိန် တူညီ‌ကြောင်း ကိုစစ်ပေးပါ။</br>");
+                        });
                 }, function error(errorMsg) {
                     showErrorMessage(errorMsg);
                 });
@@ -40,13 +56,18 @@ function init() {
 
 function showErrorMessage(msg) {
     loadingImg.style.display = "none";
+    document.getElementById("close_btn").style.display = "block";
     infoMessage.style.color = "red";
+    infoMessage.style["font-size"] = "large";
+    infoMessage.style["font-family"] = "cursive";
+    infoMessage.style["line-height"] = "40px";
     infoMessage.innerHTML = msg;
 }
 
 // this fetch params in the url dynamically;
 getUrlParam = function (name) {
-    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    // eslint-disable-next-line no-useless-escape
+    var results = new RegExp("[\?&]" + name + "=([^&#]*)").exec(window.location.href);
     if (results == null) {
         return null;
     } else {
